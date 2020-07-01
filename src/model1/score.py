a9bae2715af566bd074219294f621f6d96350392
hoemam <v-hoemam@microsoft.com>
2020-07-01 14:45:47 +0200

    reset

--- src/model1/score.py
+++ src/model1/score.py
@@ -6,9 +6,10 @@ import joblib
 from inference_schema.schema_decorators import input_schema, output_schema
 from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
 
+
 def init():
     global model
-    
+
     # Update to your model's filename
     model_filename = "model.pkl"
 
@@ -17,11 +18,12 @@ def init():
 
     print("Model dir:", model_dir)
     print("Model filename:", model_filename)
-    
+
     model_path = os.path.join(model_dir, model_filename)
 
     # Replace this line with your model loading code
     model = joblib.load(model_path)
+
 
 # Define some sample data for automatic generation of swagger interface
 input_sample = [{
@@ -34,8 +36,9 @@ input_sample = [{
     "Credit amount": 100,
     "Duration": 48,
     "Purpose": "radio/TV"
-  }]
+}]
 output_sample = [[0.7, 0.3]]
+
 
 # This will automatically unmarshall the data parameter in the HTTP request
 @input_schema('data', StandardPythonParameterType(input_sample))
@@ -44,7 +47,7 @@ def run(data):
     try:
         input_df = pd.DataFrame(data)
         proba = model.predict_proba(input_df)
-        
+
         result = {"predict_proba": proba.tolist()}
         return result
     except Exception as e:
