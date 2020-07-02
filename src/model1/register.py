a55ad0aafdd5b108e37f4ae4c61cbc80674ffd41
hoemam <v-hoemam@microsoft.com>
2020-07-02 13:20:53 +0200

    refactor register.py

--- src/model1/register.py
+++ src/model1/register.py
@@ -37,24 +37,6 @@ from azureml.core import Workspace, Experiment, Datastore
 def main():
 
     run = Run.get_context()
-    # if (run.id.startswith('OfflineRun')):
-    #     from dotenv import load_dotenv
-    #     # For local development, set values in this section
-    #     load_dotenv()
-    #     workspace_name = os.environ.get("WORKSPACE_NAME")
-    #     experiment_name = os.environ.get("EXPERIMENT_NAME")
-    #     resource_group = os.environ.get("RESOURCE_GROUP")
-    #     subscription_id = os.environ.get("SUBSCRIPTION_ID")
-    #     # run_id useful to query previous runs
-    #     run_id = "bd184a18-2ac8-4951-8e78-e290bef3b012"
-    #     aml_workspace = Workspace.get(
-    #         name=workspace_name,
-    #         subscription_id=subscription_id,
-    #         resource_group=resource_group
-    #     )
-    #     ws = aml_workspace
-    #     exp = Experiment(ws, experiment_name)
-    # else:
     ws = run.experiment.workspace
     exp = run.experiment
     run_id = 'amlcompute'
@@ -90,34 +72,18 @@ def main():
 
     print("Getting registration parameters")
 
-    # # Load the registration parameters from the parameters file
-    # with open("parameters.json") as f:
-    #     pars = json.load(f)
-    # try:
-    #     register_args = pars["registration"]
-    # except KeyError:
-    #     print("Could not load registration values from file")
-    #     register_args = {"tags": []}
+    with open("aml_config/config.json", "r") as f:
+        configs = json.load(f)
 
-    model_tags = {}
-    # for tag in register_args["tags"]:
-    #     try:
-    #         mtag = run.parent.get_metrics()[tag]
-    #         model_tags[tag] = mtag
-    #     except KeyError:
-    #         print(f"Could not find {tag} metric on parent run.")
+    datastore_data = configs['DataStore_Data']
+    model_data = configs["Model_Data"]
 
-    # load the model
-    # print("Loading model from " + model_path)
-    # model_file = os.path.join(model_path, model_name)
+    model_tags = {"name": model_data["model_name"]}
 
-    # data_store = Datastore.get_default(ws)
-    data_store = Datastore.get(ws, datastore_name='modelstore')
+    data_store = Datastore.get(ws, datastore_name=datastore_data["datastore_name"])
     data_store.download(target_path=".", prefix="model", overwrite=True)
-    print("****************************************")
-    print(os.listdir("./"))
-    print("****************************************")
-    model_file = "./model/model.pkl"
+
+    model_file = "./model/" + model_data["model_name"]
     model = joblib.load(model_file)
 
     parent_tags = run.parent.get_tags()
@@ -135,7 +101,7 @@ def main():
         print(f"Tags present: {parent_tags}")
 
     if (model is not None):
-        dataset_id = "8f63a3b1-2220-497c-aa9f-fdde6988ea0d"
+        dataset_id = model_data["dataset_id"]
         if (build_id is None):
             register_aml_model(
                 model_file,
